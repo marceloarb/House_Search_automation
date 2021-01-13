e6b2cc9714f4bb14b7af7a325f6d94e38eec3746
Marcelo Barbosa <mbarbosa@teksystems.com>
2021-01-13 13:49:23 -0600

    update

--- src/main/java/com/marcelo/view/houses/HousesView.java
+++ src/main/java/com/marcelo/view/houses/HousesView.java
@@ -25,8 +25,8 @@ public class HousesView {
        WebElement housePrice = uiHost.getSelector("#sidepane-header > div.StickyContainer-placeholder > div > div.exposedFilterForm.font-weight-roman > form > div > div.RangeSelect.quickPrice > span:nth-child(3) > span > span");
        housePrice.click();
        Select house = new Select(housePrice.findElement(By.xpath("//*[@id=\"sidepane-header\"]/div[2]/div/div[1]/form/div/div[2]/span[3]/span/span/select")));
-       uiHost.waitOneSecond();
-        house.selectByIndex(4);
+        System.out.println(house.getOptions());
+       
     }
 
 
